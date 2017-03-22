ba670f77a0af42da55047282199aa1439e1b21e0
Jeremy Cooper <jeremy@skysurfers.org>
2017-03-22 15:55:08 -0700

    Consider using a json file to keep track of levels

--- engine.py
+++ engine.py
@@ -1,13 +1,15 @@
+import json
 import pygame as pg
 from time_machine_game import TimeMachine
 from data_center_game import DataCenter
 
+# initialize pygame and the display
 pg.init()
-
 gameDisplay = pg.display.set_mode((1000, 600))
 pg.display.set_caption("Hackathon project")
 pg.display.update()
 
+# Initialize the joystick
 pg.joystick.init()
 try:
     controller = pg.joystick.Joystick(0)
@@ -15,6 +17,13 @@ try:
 except pg.error:
     print "No DS4 connected"
     controller = None
+
+# grab the game configuration
+f = open("levels.json", "r")
+
+data =json.load(f)
+f.close()
+print data
 
 # init the games
 tm = TimeMachine()
@@ -28,16 +37,17 @@ while True:
     if val != 0:
         print val
         print "\t", controller.get_hat(0)
-    # Game goes on right here?
+
+    # Grab pygame events
     events = pg.event.get()
 
-    # update the f each
+    # update the the surface of each game
     tm_surf = tm.update_ui(events)
     dc_surf = dc.update_ui(events)
 
+    # Draw each surface onto the main surface
     gameDisplay.blit(dc_surf, (0, 0))
     gameDisplay.blit(tm_surf, (350, 0))
     pg.display.flip()
     pg.time.delay(10)# smooth out the animation by adding a delay of 1/10th of a second
-    
     
