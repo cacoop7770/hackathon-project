2de7ebf3ed021466aa4c16dec97f3ed05256217f
Eric Cramer <ecramer@gaikai.com>
2017-03-22 19:14:12 -0700

    Using constants and setting up interface

--- data_center_game.py
+++ data_center_game.py
@@ -7,23 +7,13 @@ class DataCenter(Game):
 
         # The main surface is in self.main_surf
         # so want to blit my own surface there
-        self.surf = pg.Surface((350, 600))# Screen is 650x600
+        self.surf = pg.Surface((DC_W, SCREEN_H))
 
     def handle_event(self, event):
          # handle keyboard or ds4
          pass
 
-    def update_ui(self, events):
-        if self.is_active():
-            for event in events:
-                self.handle_event(event)
-
-        # draw on the surface here
-        # make sure to draw inside of self.surf
-        
-
-        # Choose background color of surface (r,g,b)
-        self.surf.fill((0, 255, 0))
-
-        # return the surface so it can be blit
+    def redraw(self):
+        """Redraw the datacenter surface and return it."""
+        self.surf.fill((255, 0, 0))
         return self.surf
