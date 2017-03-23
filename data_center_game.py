afe555b34ec2ec04843d75a9d671a1311ab1884c
Eric Cramer <ecramer@gaikai.com>
2017-03-22 19:50:27 -0700

    Moved Raj code to data_center_game.py

--- data_center_game.py
+++ data_center_game.py
@@ -1,6 +1,9 @@
 import pygame as pg
 from gui import Game
 
+white = (255,255,255)
+black = (0,0,0)
+
 class DataCenter(Game):
     def __init__(self, controller):
         Game.__init__(self, controller)
@@ -9,11 +12,37 @@ class DataCenter(Game):
         # so want to blit my own surface there
         self.surf = pg.Surface((DC_W, SCREEN_H))
 
+        ### EDIT CODE1 BELOW ###
+
+        self.gameExit = False
+
+        self.lead_x = 300
+        self.lead_y = 300
+
+        ### EDIT CODE1 ABOVE ###
+
+
     def handle_event(self, event):
-         # handle keyboard or ds4
-         pass
+         """handle keyboard or ds4"""
+
+         ### EDIT CODE3 BELOW ###
+
+        if event.type == pg.QUIT:
+            pygame.quit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                self.lead_x -= 10
+            if event.key == pg.K_RIGHT:
+                self.lead_x += 10
+
+        ### EDIT CODE3 ABOVE ###
+
 
     def redraw(self):
         """Redraw the datacenter surface and return it."""
-        self.surf.fill((255, 0, 0))
+        ### EDIT CODE2 BELOW ###
+        self.surf.fill(white)
+        pg.draw.rect(self.surf, black, [lead_x, lead_y, 10, 10])
+        ### EDIT CODE2 ABOVE ###
+
         return self.surf
